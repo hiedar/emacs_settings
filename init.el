9b0dfb474fe90f7db759b260985b406b2bcecb52
ryuichi <hieda.ryuichi@lib-gate.co.jp>
2014-08-10 23:42:04 +0900

    new settings

---
+++ init.el
@@ -0,0 +1,202 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;; DEFINE FUNCTIONS ;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; [DEFINE FUNCTION]
+;;  add-to-load-path
+;;  ロードパス(拡張機能、設定ファイルの探索をするためのディレクトリ)の設定
+;;  サブディレクトリもロードパスに追加する関数
+(defun add-to-load-path (&rest paths)
+  (let (path)
+    (dolist (path paths paths)
+      (let ((default-directory
+              (expand-file-name (concat user-emacs-directory path))))
+        (add-to-list 'load-path default-directory)
+        (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+            (normal-top-level-add-subdirs-to-load-path))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;; PACKAGE.EL ;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;; MAGICAL SPELL ;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(require 'cl)
+
+;; Emacsからの質問をy/nで回答する
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; スタートアップメッセージを非表示
+(setq inhibit-startup-screen t)
+
+(when window-system
+  ;; tool-barを非表示
+  (tool-bar-mode 0))
+
+;;  Emacs.app(OS X)のための環境変数の設定
+;;  （Terminal上では引き継がれているはず）
+;; (add-to-list 'exec-path "/opt/local/bin")
+;; (add-to-list 'exec-path "/usr/local/bin")
+;; (add-to-list 'exec-path "~/bin")
+
+;; load-path
+(add-to-load-path "elisp" "conf" "public_repos" "elpa")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;; LANGUAGE SETTINGS ;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; [DEFINE LANGUAGE CODE]
+(set-language-environment "Japanese")
+(prefer-coding-system 'utf-8)
+
+;; [HANDLE FILE NAME]
+;;  OS X
+(when (eq system-type 'darwin)
+  (require 'ucs-normalize)
+  (set-file-name-coding-system 'utf-8-hfs)
+  (setq locale-coding-system 'utf-8-hfs))
+
+;;  Windows
+(when (eq system-type 'w32)
+  (set-file-name-coding-system 'cp932)
+  (setq locale-coding-system 'cp932))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;; KEY MAPS ;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;  new line and indent
+(define-key global-map (kbd "C-m") 'newline-and-indent)
+
+;;  backspace
+(keyboard-translate ?\C-h ?\C-?)
+
+;;  折り返しを簡単に切り替える
+(define-key global-map (kbd "C-c l") 'toggle-truncate-lines)
+
+;;  ウインドウを切り替える
+(define-key global-map (kbd "C-t") 'other-window)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;; MODE LINE ;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; display column number
+(column-number-mode t)
+
+;; display file size
+(size-indication-mode t)
+
+;; display row number
+(global-linum-mode t)
+
+;; SPECIAL display full path of file on  title bar
+(setq frame-title-format "%f")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;; TAB ;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(setq-default tab-width 4)
+
+;; don't use tab character
+(setq-default indent-tabs-mode nil)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;; LOOKING ;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(set-cursor-color 'palegreen1)
+(defface my-hl-line-face
+  ;; 背景がdarkならば背景色を紺に
+  '((((class color) (background dark))
+     (:background "dark" t))
+    ;; 背景がlightならば背景色を緑に
+    (((class color) (background light))
+     (:background "LightGoldenrodYellow" t))
+    (t (:bold t)))
+  "hl-line's my face")
+(setq hl-line-face 'my-hl-line-face)
+(global-hl-line-mode t)
+
+(set-face-attribute 'default nil
+                    :family "Source Code Pro"
+                    :height 120)
+
+(set-fontset-font
+ nil 'japanese-jisx0208
+ (font-spec :family "ヒラギノ角ゴ Pro"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;; AUTO INSTALL ;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(when (require 'auto-install nil t)
+  (setq auto-install-directory "~/.emacs.d/elisp/")
+  (auto-install-update-emacswiki-package-name t)
+  (auto-install-compatibility-setup))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;; MIGEMO ;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; (参考) http://weblog.ymt2.net/blog/html/2013/08/23/install_migemo_to_emacs_24_3_1.html
+
+(require 'migemo)
+(setq migemo-command "/usr/local/bin/cmigemo")
+(setq migemo-options '("-q" "--emacs"))
+(setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
+(setq migemo-user-dictionary nil)
+;; Windowsの場合のcodingシステムを変更する必要あり？
+(setq migemo-coding-system 'utf-8-unix)
+(setq migemo-regex-dictionary nil)
+(load-library "migemo")
+(migemo-init)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;; SEQUENTIAL COMMAND ;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;  package-install sequential-commandでインストール
+(require 'sequential-command-config)
+(sequential-command-setup-keys)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;; RECTANGLE EDIT ;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(cua-mode t)
+(setq cua-enable-cua-keys nil)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;; ELSCREEN ;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;  package-install elscreenでインストール
+(require 'elscreen)
+(elscreen-start)
+(elscreen-set-prefix-key (kbd "C-;"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;; WDIRED.EL ;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define-key dired-mode-map "r" 'wdired-change-to-wdired-mode)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;; INSERT TEMPLATE ;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(auto-insert-mode)
+(setq auto-insert-directory "~/.emacs.d/auto-insert/")
+;; html
+(define-auto-insert "\\.html$" "html-template.html")

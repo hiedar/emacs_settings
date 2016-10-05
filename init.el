f616415ec3e0a9a4fab2f9d4389cdbfd55c1a887
Ryuichi Hieda <ryuichi.hieda@gmail.com>
2016-10-05 18:24:16 +0900

    update init.el.

--- init.el
+++ init.el
@@ -7,6 +7,10 @@
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (package-initialize)
+(setq package-archives
+      '(("gnu" . "http://elpa.gnu.org/packages/")
+        ("melpa" . "http://melpa.org/packages/")
+        ("org" . "http://orgmode.org/elpa/")))
 
 ;;; 右から左に読む言語に対応させないことで描画高速化
 (setq-default bidi-display-reordering nil)
@@ -23,9 +27,8 @@
 ;;; 複数のディレクトリで同じファイル名のファイルを開いたときのバッファ名を調整する
 (require 'uniquify)
 
-;; filename<dir> 形式のバッファ名にする
-(setq uniquify-buffer-name-style 'post-forward-angle-brackets)
-(setq uniquify-ignore-buffers-re "[^*]+")
+;;; リージョンを削除できるように
+(delete-selection-mode t)
 
 ;;; ファイルを開いた位置を保存する
 (require 'saveplace)
@@ -54,6 +57,11 @@
 (line-number-mode 1)
 (column-number-mode 1)
 
+;;; 画面横に行番号を表示する
+(require 'linum)
+(global-linum-mode t)      ; デフォルトで linum-mode を有効にする
+(setq linum-format "%5d ") ; 5 桁分の領域を確保して行番号のあとにスペースを入れる
+
 ;;; GCを減らして軽くする
 (setq gc-cons-threshold (* 10 gc-cons-threshold))
 
@@ -77,7 +85,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (ace-jump-mode migemo))))
+ '(package-selected-packages
+   (quote
+    (recentf-ext helm markdown-mode elscreen-persist elscreen smart-tab ace-jump-mode migemo))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -111,3 +121,118 @@
 ;;; window
 (global-set-key (kbd "C-t") 'other-window)
 
+;;; Ohter keybind.
+;; M-k でカレントバッファを閉じる
+(define-key global-map (kbd "M-k") 'kill-this-buffer)
+;; Mac の Command + 3 でウィンドウを上下に分割
+(define-key global-map (kbd "s-3") 'split-window-vertically)
+;; Mac の command + 2 でウィンドウを左右に分割
+(define-key global-map (kbd "s-2") 'split-window-horizontally)
+;; Mac の Command + 1 で現在のウィンドウ以外を閉じる
+(define-key global-map (kbd "s-1") 'delete-other-windows)
+;; Mac の Command + 0 で現在のウィンドウを閉じる
+(define-key global-map (kbd "s-0") 'delete-window)
+
+;;; 改行やタブを可視化する whitespace-mode
+(setq whitespace-display-mappings
+      '((space-mark ?\x3000 [?\□]) ; zenkaku space
+        (newline-mark 10 [8629 10]) ; newlne
+        (tab-mark 9 [187 9] [92 9]) ; tab » 187
+        )
+      whitespace-style
+      '(spaces
+        ;; tabs
+        trailing
+        newline
+        space-mark
+        tab-mark
+        newline-mark))
+;; whitespace-modeで全角スペース文字を可視化　
+(setq whitespace-space-regexp "\\(\x3000+\\)")
+;; whitespace-mode をオン
+(global-whitespace-mode t)
+
+;;; PATH設定
+;; http://sakito.jp/emacs/emacsshell.html#path
+;; より下に記述した物が PATH の先頭に追加されます
+(dolist (dir (list
+              "/sbin"
+              "/usr/sbin"
+              "/bin"
+              "/usr/bin"
+              "/opt/local/bin"
+              "/sw/bin"
+              "/usr/local/bin"
+              (expand-file-name "~/bin")
+              (expand-file-name "~/.emacs.d/bin")
+              ))
+;; PATH と exec-path に同じ物を追加します
+ (when (and (file-exists-p dir) (not (member dir exec-path)))
+   (setenv "PATH" (concat dir ":" (getenv "PATH")))
+   (setq exec-path (append (list dir) exec-path))))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; elscreen ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; プレフィクスキーはC-z
+(setq elscreen-prefix-key (kbd "C-z"))
+(elscreen-start)
+(elscreen-persist-mode 1)
+;;; タブの先頭に[X]を表示しない
+(setq elscreen-tab-display-kill-screen nil)
+;;; header-lineの先頭に[<->]を表示しない
+(setq elscreen-tab-display-control nil)
+; 新規スクリーン作成
+(global-set-key (kbd "s-t") 'elscreen-create)
+;; スクリーンとバッファをkill
+(global-set-key (kbd "s-w") 'elscreen-kill-screen-and-buffers)
+;;ブラウザみたいにタブ移動
+(global-set-key [(C-tab)] 'elscreen-next) 
+(global-set-key [(C-S-tab)] 'elscreen-previous)
+;; diredを新しいscreenで
+(global-set-key (kbd "C-x C-d") 'elscreen-dired)
+
+;;; バッファ名・モード名からタブに表示させる内容を決定する(デフォルト設定)
+(setq elscreen-buffer-to-nickname-alist
+      '(("^dired-mode$" .
+         (lambda ()
+           (format "Dired(%s)" dired-directory)))
+        ("^Info-mode$" .
+         (lambda ()
+           (format "Info(%s)" (file-name-nondirectory Info-current-file))))
+        ("^mew-draft-mode$" .
+         (lambda ()
+           (format "Mew(%s)" (buffer-name (current-buffer)))))
+        ("^mew-" . "Mew")
+        ("^irchat-" . "IRChat")
+        ("^liece-" . "Liece")
+        ("^lookup-" . "Lookup")))
+(setq elscreen-mode-to-nickname-alist
+      '(("[Ss]hell" . "shell")
+        ("compilation" . "compile")
+        ("-telnet" . "telnet")
+        ("dict" . "OnlineDict")
+        ("*WL:Message*" . "Wanderlust")))
+
+;;; helm
+(require 'helm-config)
+(helm-mode 1)
+(helm-migemo-mode 1)
+
+;; C-hで前の文字削除
+(define-key helm-map (kbd "C-h") 'delete-backward-char)
+(define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
+(global-set-key (kbd "M-h") 'helm-mini)
+;; ミニバッファでC-hをバックスペースに割り当て
+;;(define-key helm-map (kbd "C-h") 'delete-backward-char)
+
+;;; ffap
+(ffap-bindings)
+
+;;; recentf関連
+;; 最近のファイル500個を保存する
+(setq recentf-max-saved-items 500)
+;; 最近使ったファイルに加えないファイルを
+;; 正規表現で指定する
+(setq recentf-exclude
+      '("/TAGS$" "/var/tmp/"))
+(require 'recentf-ext)
